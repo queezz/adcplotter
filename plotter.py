49972995d1e8ddb7f8328875a2cd44b1b8cee4cd
pi <hasuo_kuzmin.lab@outlook.jp>
2026-04-23 11:15:41 +0900

    add channels and colors

--- plotter.py
+++ plotter.py
@@ -8,6 +8,7 @@ import pyqtgraph as pg
 from AIO import AIO_32_0RA_IRC
 
 NCHANNELS = 32
+MAX_POINTS = 1000
 
 
 class ADCWorker(QtCore.QObject):
@@ -39,7 +40,8 @@ class ADCWorker(QtCore.QObject):
         data["Timestamp"] = now
         data["t"] = pd.Timedelta(now - self.start_time).total_seconds()
         for channel in range(NCHANNELS):
-            data["Channel {}".format(channel)] = self.adc.analog_read_volt(channel, self.data_rate)
+            data[f"Channel {channel}"] = self.adc.analog_read_volt(channel, self.data_rate)
+
         df = pd.DataFrame(data, index=[0])
         self.data_ready.emit(df)
         self.app.processEvents()
@@ -73,20 +75,53 @@ class MainWindow(QtWidgets.QMainWindow):
         self.plot_widget = pg.PlotWidget()
         self.plot_widget.setLabel("left", "Voltage", units="V")
         self.plot_widget.setLabel("bottom", "Time", units="s")
-        self.plot_curves = [self.plot_widget.plot() for _ in range(NCHANNELS)]
+        self.plot_widget.showGrid(x=True, y=True)
+        self.plot_widget.enableAutoRange()
+
+        self.plot_curves = [
+            self.plot_widget.plot(pen=pg.intColor(i, hues=NCHANNELS))
+            for i in range(NCHANNELS)
+        ]
 
         layout.addWidget(self.plot_widget)
 
+        # Channel selector
+        self.channel_selector = QtWidgets.QComboBox()
+        self.channel_selector.addItem("All")
+        for i in range(NCHANNELS):
+            self.channel_selector.addItem(f"Channel {i}")
+        layout.addWidget(self.channel_selector)
+
         self.start_stop_button = QtWidgets.QPushButton("Start")
         layout.addWidget(self.start_stop_button)
+
         self.center()
 
     @QtCore.pyqtSlot(pd.DataFrame)
     def update_plot(self, data):
         self.plot_data = pd.concat([self.plot_data, data], ignore_index=True)
-        for channel in range(NCHANNELS - 1):
-            self.plot_curves[channel].setData(self.plot_data["t"], self.plot_data[f"Channel {channel}"])
-        # self.plot_curves[3].setData(self.plot_data["t"], self.plot_data[f"Channel {3}"] / 10 / 1e-3)
+
+        if len(self.plot_data) > MAX_POINTS:
+            self.plot_data = self.plot_data.iloc[-MAX_POINTS:]
+
+        selected = self.channel_selector.currentText()
+
+        if selected == "All":
+            for ch in range(NCHANNELS):
+                self.plot_curves[ch].setVisible(True)
+                self.plot_curves[ch].setData(
+                    self.plot_data["t"],
+                    self.plot_data[f"Channel {ch}"]
+                )
+        else:
+            ch = int(selected.split()[-1])
+            for i in range(NCHANNELS):
+                self.plot_curves[i].setVisible(i == ch)
+
+            self.plot_curves[ch].setData(
+                self.plot_data["t"],
+                self.plot_data[f"Channel {ch}"]
+            )
 
     def del_thread(self):
         self.adc_thread.quit()
